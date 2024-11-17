ec4ee86c35c17d9c9da3c468911cb5d32f40c85d
JoãoSousa <91sousajoao@gmail.com>
2024-11-17 11:26:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Sobre.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppHotel.Views
+{
+    public partial class Sobre : ContentPage
+    {
+        public Sobre()
+        {
+            InitializeComponent();
+        }
+    }
+}
